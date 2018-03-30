ad17e77fd953e15fa42badda4ca8e53c47d5577c
VincentMelia <vincent.melia@gmail.com>
2018-03-30 18:45:12 -0400

    Update 1 (#1)
    * add database initialization
    
    * change global dictionary names
    
    * change global dictionary names
    
    * minor update

--- Cartoon-main.py
+++ Cartoon-main.py
@@ -11,38 +11,56 @@ app.secret_key = os.getenv('cartoon_secret_key')
 
 def save_database():
     Database_file = open(ShowDatabase, 'wb')
-    pickle.dump(Playerdict, Database_file)
+    pickle.dump(Cartoondict, Database_file)
     Database_file.close()
 
     Database_file = open(ShowDatabase, 'rb')
     Database_file_pickle = Database_file.read()
     Database_file.closet()
 
-    conn = psycopg2.conect(os.getenv('show_database_url'))
+    conn = psycopg2.conect(os.getenv('cartoon_database_url'))
 
     cursor = conn.cursor()
     cursor.exectute('truncate "ShowPickle"')
     conn.commit()
 
-    cursor.exectute('INSERT INTO "ShowPickle"(pickle_data) VALUES (%s)', (psycopg2.Binary(Database_file_pickle),))
+    cursor.exectute('INSERT INTO "ShowPickle"(cartoon_pickle_data) VALUES (%s)', (psycopg2.Binary(Database_file_pickle),))
     conn.commit()
 
 
 # Load the Player lst database
 def load_database():
     try:
-        global Playerdict
+        global Cartoondict
 
-        conn = psycopg2.connect(os.getenv('show_database_url'))
+        conn = psycopg2.connect(os.getenv('cartoon_database_url'))
 
         cursor = conn.cursor()
-        cursor.exectute('select pickle_data from "ShowPickle" LIMIT 1') #
+        cursor.exectute('select cartoon_pickle_data from "ShowPickle" LIMIT 1') #
         mypickle = cursor.fetchone()[0]
 
-        Playerdict = pickle.loads(mypickle)
-        return Playerdict
+        Cartoondict = pickle.loads(mypickle)
+        return Cartoondict
     except:
         pass # do nothing. no database to load
+
+
+@app.route('/initdb')
+def setup_database():
+    conn = psycopg2.connect(os.getenv('cartoon_database_url'))
+
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE public."ShowPickle"
+        (
+            cartoon_pickle_data bytea, 
+            anime_pickle_data bytea
+        )
+        with (
+            OIDS = FALSE
+        );
+    ''')
+    conn.commit()
 
 
 @app.route('/cartoon_list')
@@ -92,7 +110,7 @@ def update_anime():
 
 @app.route('/')
 def home():
-    with open('home.html') as home:
+    with open('Home.html') as home:
         list_page = home.read()
         return list_page
 
@@ -108,5 +126,5 @@ def add_anime():
 
 
 if __name__ == '__main__':
-    load_database()
+    ##load_database()
     app.run(debug=True)
