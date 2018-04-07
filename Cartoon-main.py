434c11f9208b61489df5125c56d6b5fde85c96d3
Joseph Cassinera <>
2018-04-07 14:33:49 -0400

    added cartoon edit and post service

--- Cartoon-main.py
+++ Cartoon-main.py
@@ -3,10 +3,13 @@ import os
 import uuid
 import psycopg2
 from Configuration import ShowDatabase
-from flask import Flask, request, redirect, send_from_directory
+from flask import Flask, request, redirect, send_from_directory, Response, render_template
+from Show_Objects import cartoon_show_object
 
 app = Flask(__name__)
 app.secret_key = os.getenv('cartoon_secret_key')
+
+Cartoondict = {}
 
 
 def save_database():
@@ -24,7 +27,8 @@ def save_database():
     cursor.exectute('truncate "ShowPickle"')
     conn.commit()
 
-    cursor.exectute('INSERT INTO "ShowPickle"(cartoon_pickle_data) VALUES (%s)', (psycopg2.Binary(Database_file_pickle),))
+    cursor.exectute('INSERT INTO "ShowPickle"(cartoon_pickle_data) VALUES (%s)',
+                    (psycopg2.Binary(Database_file_pickle),))
     conn.commit()
 
 
@@ -36,13 +40,13 @@ def load_database():
         conn = psycopg2.connect(os.getenv('cartoon_database_url'))
 
         cursor = conn.cursor()
-        cursor.exectute('select cartoon_pickle_data from "ShowPickle" LIMIT 1') #
+        cursor.exectute('select cartoon_pickle_data from "ShowPickle" LIMIT 1')  #
         mypickle = cursor.fetchone()[0]
 
         Cartoondict = pickle.loads(mypickle)
         return Cartoondict
     except:
-        pass # do nothing. no database to load
+        pass  # do nothing. no database to load
 
 
 @app.route('/initdb')
@@ -111,13 +115,22 @@ def update_anime():
 @app.route('/')
 def home():
     with open('Home.html') as home:
-        list_page = home.read()
+        list_page = open('Home.html').read()
         return list_page
 
 
-@app.route('/cartoon/new')
+@app.route('/cartoon/new', methods=['POST',])
 def add_cartooon():
-    pass
+    # instantiate a new show object and populate it from request.form
+    New_Cartoon = cartoon_show_object(
+        cartoon_show_object(showname=request.form['Title_Input']),
+        cartoon_show_object(showimage=request.form['Image_Input']),
+        cartoon_show_object(showlink=request.form['link_Input'])
+    )
+
+    Cartoondict[New_Cartoon.id] = New_Cartoon
+    save_database()
+    return redirect('/')
 
 
 @app.route('/anime/new')
@@ -126,5 +139,5 @@ def add_anime():
 
 
 if __name__ == '__main__':
-    ##load_database()
+    load_database()
     app.run(debug=True)
--- Show_Objects.py
+++ Show_Objects.py
@@ -2,14 +2,16 @@ import uuid
 
 
 class anime_show_object:
-    def __init__(self, showname='', showimage=None, showlink=''):
+    def __init__(self, showname='', showimage=None, showlink='', id=uuid.uuid4()):
+        self.id=id
         self.showname=showname
         self.showimage=showimage
         self.showlink=showlink
 
 
 class cartoon_show_object:
-    def __init__(self, showname='', showimage=None, showlink=''):
+    def __init__(self, showname='', showimage=None, showlink='', id=uuid.uuid4()):
+        self.id=id
         self.showname=showname
         self.showimage=showimage
         self.showlink=showlink
