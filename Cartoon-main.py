c6ae725bfb7d1fe4df95c6e4996c3139f7aeaf3e
Vincent Melia <>
2018-05-12 11:45:42 -0400

    home page fixes

--- Cartoon-main.py
+++ Cartoon-main.py
@@ -114,8 +114,10 @@ def update_anime():
 
 @app.route('/')
 def home():
-    with open('Home.html') as home:
-        list_page = open('Home.html').read()
+    THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+    home_page = os.path.join(THIS_FOLDER, 'Home.html')
+    with open(home_page) as home:
+        list_page = open(home_page).read()
         return list_page
 
 
